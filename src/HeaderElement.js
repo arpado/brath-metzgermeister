f69a4aaa706352e92e1bfbb79dfa3c06c104dc05
arpado <ozsvar.arpad@gmail.com>
2022-10-24 09:51:27 +0200

    init commit

---
+++ src/HeaderElement.js
@@ -0,0 +1,17 @@
+const HeaderElement = () => {
+    return (
+        <header>
+            <div className="logo-major">
+
+            </div>
+            <div className="header-top">
+
+            </div>
+            <div className="header-bottom">
+                
+            </div>
+        </header>
+    );
+}
+ 
+export default HeaderElement;
